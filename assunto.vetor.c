10f95a9509e610c55a0cc31c97e3f5d60804617c
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-03-14 16:40:04 -0300

    Add files via upload

---
+++ assunto.vetor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+ int main () {
+ 	 
+ 	int vetor; 
+ 	int i;
+ 	
+ 	 printf ("digite os elementos do vetor:\n");
+ 	 for (i = 0; i<5; i++) {
+         printf ("Elementos %d: ", i+1, vetor);
+         scanf ("%d",&vetor);
+     }
+     
+     //exinbindo os dados do vetor em um laço de repetição
+      for (i = 0; i<5; i++) {
+     printf ("digite os elementos do vetor:\n");
+ 
+     
+}
+return 0;
+
+ } 
